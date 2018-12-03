9f46430adc6d54b3df25bdaf086adb04e66ff953
Scott Sedgwick <ssedgwick@cochlear.com>
2018-12-03 17:15:34 +1100

    Completed Day 3

--- test/TestDay3.hs
+++ test/TestDay3.hs
@@ -5,7 +5,6 @@ import Lib
 
 mainDay3 :: IO()
 mainDay3 = hspec $ do
-  describe "testing" $ do
+  describe "overlap" $ do
     it "should run" $ do
-      d3a1 `shouldBe` 0
-      d3a2 `shouldBe` 0
+      overlap (1,1,3,4,4) (2,3,1,4,4) `shouldBe` 4
